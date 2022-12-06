148fcafd9d4608f83f9551e99795aaf362a1e8f4
AidanSchmid <aschmid@quinnipiac.com>
2022-12-06 04:56:48 -0500

    Updated Http service

--- part-production-dashboard/src/services/httpService.js
+++ part-production-dashboard/src/services/httpService.js
@@ -1,5 +1,5 @@
 import http from "./httpService";
-const apiUrl = "http://localhost:4857/"
+const apiUrl = "http://localhost:4857/";
 const apiEndpoint = apiUrl + "user";
 
 /**
@@ -8,19 +8,19 @@ const apiEndpoint = apiUrl + "user";
  * @param {Object} data The login information
  * @return {Object} A JWT token
  */
- export function login(data) {
-    return http.post(`${apiEndpoint}/login`, data);
-  }
+export function login(data) {
+  return http.post(`${apiEndpoint}/login`, data);
+}
 
-/** 
+/**
  * Posts the login data
- * 
+ *
  * @param {String} userId the id of the user
  * @return {Object} A JWT token
  */
 export function logout(userId) {
-    return http.post(`${apiEndpoint}/${userId}/logout`);
-  }
+  return http.post(`${apiEndpoint}/${userId}/logout`);
+}
 
 /**
  * Creates a user account
@@ -28,26 +28,96 @@ export function logout(userId) {
  * @param {Object} data The information to create an account
  * @return {Object} The created account
  */
- export function createAccount(data) {
-    return http.post(`${apiEndpoint}/createAccount`, data);
-  }
+export function createAccount(data) {
+  return http.post(`${apiEndpoint}/createAccount`, data);
+}
 
 /**
  * Gets all the user accounts in the database
  *
  * @return {Object} A list of accounts in the databse
  */
- export function getUsers() {
-    return http.get(`${apiEndpoint}/users`);
-  }
+export function getUsers() {
+  return http.get(`${apiEndpoint}/users`);
+}
 
 /**
  * Creates a tracking entry in the database
- * 
+ *
  * @param {data} The information of the tracking entry
  * @return {Object} The created tracking entry
  */
 
 export function createTracking(data) {
-    return http.post(`${apiEndpoint}/tracking`, data);
-  } 
+  return http.post(`${apiEndpoint}/tracking`, data);
+}
+
+// Creates a New Product
+export function products(data) {
+  return http.post(`${apiEndpoint}/products`, data);
+}
+
+// Products get by id
+export function productId(data) {
+  return http.get(`${apiEndpoint}/products/:id`, data);
+}
+
+// Good Products
+export function goodProducts(data) {
+  return http.get(
+    `${apiEndpoint}/products/:id/good/:tracking/:date/:area:/shift/:operator`,
+    data
+  );
+}
+
+// Bad Products
+export function badProducts(data) {
+  return http.get(
+    `${apiEndpoint}/products/:id/bad/:tracking/:date/:area:/shift/:operator`,
+    data
+  );
+}
+
+// Target Products
+export function targetProducts(data) {
+  return http.get(
+    `${apiEndpoint}/products/:id/target/:tracking/:date/:area:/shift/:operator`,
+    data
+  );
+}
+
+// Actual Products
+export function actualProducts(data) {
+  return http.get(
+    `${apiEndpoint}/products/:id/actual/:tracking/:date/:area:/shift/:operator`,
+    data
+  );
+}
+
+// Finished Products
+export function finishedProducts(data) {
+  return http.get(
+    `${apiEndpoint}/products/:id/finished/:tracking/:date/:area:/shift/:operator`,
+    data
+  );
+}
+
+// Create Safety report
+export function safety(data) {
+  return http.post(`${apiEndpoint}/safety`, data);
+}
+
+// Return all safety reports
+export function getSafety(data) {
+  return http.post(`${apiEndpoint}/safety/:type/:area/:date`, data);
+}
+
+// Tracking
+export function track(data) {
+  return http.post(`${apiEndpoint}/tracking`, data);
+}
+
+// Get Tracking
+export function tracking(data) {
+  return http.get(`${apiEndpoint}/tracking/:date/:area/:shift/:operator`, data);
+}
