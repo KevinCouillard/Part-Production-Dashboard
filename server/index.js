7a0d8a41e4d4f99eefb18aefdcbfa58671f87715
KevinCouillard <kevincouillard4@gmail.com>
2022-12-07 17:46:58 -0500

    Database Connection (DNF)

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config({ path: "../.env" });
 const express = require ('express');
 const userRouter = require("./routes/users");
+const mysql = require("mysql");
 
 const app = express();
 
@@ -13,6 +14,42 @@ const app = express();
 // passport.use(User.createStrategy());
 // passport.serializeUser(User.serializeUser());
 // passport.deserializeUser(User.deserializeUser());
+
+const db = mysql.createPool({
+  host     : '32.27.177.164',
+  user     : 'root',
+  password : '/5NXS-LDMySlpuXI',
+  database : 'parker_db'
+});
+
+// db.connect(function(err) {              
+//   if(err) {                                    
+//     console.log('error when connecting to db:', err);
+//   }                                     
+// });
+const email = "email@test.com";
+const password = "password";
+const IsManager = true;
+const test = "INSERT INTO User (email, password, isManager) VALUES ('email@test.com', 'password', 'true')";
+
+//test but timed out error thrown on connection for me
+db.getConnection(function(err, connection) 
+{
+    if (err) 
+    {
+         console.log("Connection ERROR")
+         console.log(err);
+    }
+    else 
+    {
+      connection.query(test, [email, password, IsManager], function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("successfully inserted");
+        }})
+    }
+});
 
 // Routes
 app.use("/user", userRouter);
